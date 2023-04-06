876e4a25fbd9548b0daab9137956d9ce5fd58240
harenohee <82671414+harenohee@users.noreply.github.com>
2023-04-06 16:39:54 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    let answer = [];
+    const arr = s.split(" ");
+    answer.push(Math.min(...arr));
+    answer.push(Math.max(...arr));
+  
+    return answer.join(" ");
+}
