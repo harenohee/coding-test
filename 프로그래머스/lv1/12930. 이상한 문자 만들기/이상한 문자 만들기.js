232fd66654e2ed4855363613de40026d11bf5025
harenohee <82671414+harenohee@users.noreply.github.com>
2023-05-19 16:06:22 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.21 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,20 +1,20 @@
 function solution(s) {
-    var array = s.split(' ');
-    var result ="";
-    var result2 = [];
-
-
-    array.forEach(function(item){
-        for(let i = 0; i<item.length; i++){
-            if(i%2==0){
-                result += item[i].toUpperCase()
-            }else{
-                result += item[i].toLowerCase()
-            }
-        }
-        result2.push(result) ;
-        result = "";
-    })
-    let answer = result2.join([" "]);
-    return answer;
+  const arr = s.split(" ");
+  const result = arr.map((i) => {
+    let str = ""
+      for(idx=0; idx<i.length; idx++){
+          if (idx % 2 == 0) {
+        // i = i.replace(i[idx], i[idx].toUpperCase());
+              str += i[idx].toUpperCase()
+      }
+          if (idx % 2 == 1) {
+        // i = i.replace(i[idx], i[idx].toLowerCase());
+              str += i[idx].toLowerCase()     
+      }
 }
+    return str;
+  });
+    
+  return result.join(" ");
+}
+
